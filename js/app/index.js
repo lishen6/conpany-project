62e0dcaaab4562ecb6f48d6389a4dcd534bb271e
唐琪琪 <976940060@qq.com>
2017-08-15 00:25:15 +0800

    no message

---
+++ js/app/index.js
@@ -0,0 +1,8 @@
+/* 动态换算rem和px值  */
+var rem =20;
+function changeW() {
+    rem = 20 / 375 * document.documentElement.clientWidth;
+    document.documentElement.style.fontSize = rem + "px";
+}
+changeW();
+window.addEventListener("resize", changeW, false);
